a36ba8e30e96dd3125ad57558005780be542b121
louis <farina@etud.insa-toulouse.fr>
2021-11-02 01:11:36 +0100

    First version

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-# SantaBOT
+**Secret Santa Bot**
+
+This bot matches people for a Secret Santa by taking their infos from a Google Sheet (which you can create through a form) and DMing them automatically on Discord.
+
+**Sheet format**
+
+Column B (starting at B2) should contain participants' usernames and discriminants (xyzxyz#1234)
+
+Column C (starting at C2) should contain their address
+
+Column D (starting at D2) should contain an (optional) message from giftees to their Santa
+
+**How to use it?**
+
+1-Install discord.py and the Python Google API : ``pip install discord`` and ``pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib``
+
+2-Create a project on [Google Cloud](https://console.cloud.google.com/home/dashboard?project=santabot-330812&hl=fr).
+
+3-Go to "API and Services", then browse the library and add the Google Sheet API.
+
+4-Add a service account, and generate a json key for that account. Put that json in the same file as the script.
+
+5-Copy the sheet's id into the code (the link should look like docs.google.com/spreadsheets/d/**xxxxxxxxxxxxxxxxxxxxxxxxxxxx**/edit[...], just copy the part in bold). Make sure you give at least read access to people with the link to your sheet.
+
+6-Go to the [Discord Developer Portal](https://discord.com/developers/applications) and create a new application.
+
+7-Copy the bot's token into the code.
+
+8-Invite the bot to a server with all the participants.
+
+9-Run the bot with ``python main.py``
---
+++ main.py
@@ -0,0 +1,75 @@
+import discord
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow,Flow
+from google.auth.transport.requests import Request
+from google.oauth2 import service_account
+import os
+import random
+
+intents = discord.Intents.default()
+intents.members = True
+client = discord.Client(intents=intents)
+
+SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+key_file = 'your-key.json'#replace with the name of your Google API key file
+
+#ENTER THE ID OF YOUR GOOGLE SHEET HERE
+SAMPLE_SPREADSHEET_ID_input = 'your-sheet-id'
+SAMPLE_RANGE_NAME = 'B2:D'
+
+creds = service_account.Credentials.from_service_account_file(key_file, scopes=SCOPES) # here enter the name of your downloaded JSON file
+
+service = build('sheets', 'v4', credentials=creds)
+
+# Call the Sheets API
+sheet = service.spreadsheets()
+result_input = sheet.values().get(spreadsheetId=SAMPLE_SPREADSHEET_ID_input,
+                                range=SAMPLE_RANGE_NAME).execute()
+    
+names = [line[0] for line in  result_input.get('values', [])]
+addresses = [line[1] for line in  result_input.get('values', [])]
+messages = [line[2] for line in  result_input.get('values', [])]
+
+"""
+print(names)
+print(addresses)
+print(messages)
+"""
+
+if not names:
+    print('No data found.')
+
+n = len(names)
+
+ok = False
+while not ok:
+    giftees=[i for i in range(n)]
+    ok = True
+    for i in range(n-1):
+        j = random.randint(i,n-1)
+        if giftees[j] == i:
+            ok = False
+            break
+        else:
+            giftees[i],giftees[j] = giftees[j], giftees[i]
+    if giftees[n-1] == n-1:
+        ok = False
+
+@client.event
+async def on_ready():
+    print('We have logged in as {0.user}'.format(client))
+    #for m in client.get_all_members():
+    #    print(m)
+    for i in range(n):
+        user = discord.utils.get(client.get_all_members(), name=names[i][:-5], discriminator=names[i][-4:])
+        if user is None:
+            print("user " + user + " not found")
+        else:
+            await user.send("Ton bébé Noël est ||" + names[giftees[i]] + "|| !\n"
+                          + "Son adresse est || " + addresses[giftees[i]] + "||\n"
+                          + "Il t'a laissé le message suivant : ||" + messages[giftees[i]] + "||")
+                             
+
+#ENTER THE TOKEN OF YOUR DISCORD BOT HERE
+client.run("your-bot-token")
+
