364ffa63036d1265c50b15555650a87e3e79534d
louis <farina@etud.insa-toulouse.fr>
2021-11-02 09:02:40 +0100

    Discord client now closes after all the DMs are sent

--- main.py
+++ main.py
@@ -68,7 +68,7 @@ async def on_ready():
             await user.send("Your giftee is ||" + names[giftees[i]] + "|| !\n"
                           + "Their address is || " + addresses[giftees[i]] + "||\n"
                           + "They left the following message for you : ||" + messages[giftees[i]] + "||")
-                             
+    client.close()                             
 
 #ENTER THE TOKEN OF YOUR DISCORD BOT HERE
 client.run("your-bot-token")
