43a0f6c5c0564fce935f01df04ddc83585ea8748
louis <farina@etud.insa-toulouse.fr>
2021-11-08 02:50:05 +0100

    Added new matching algorithm + checks for failure to DM users

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-import discord
 from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow,Flow
 from google.auth.transport.requests import Request
 from google.oauth2 import service_account
-import os
+import discord
 import random
 import sys
+import numpy as np
 
 from config import Config
+from maxflow import maxflow
 
 intents = discord.Intents.default()
 intents.members = True
 client = discord.Client(intents=intents)
 
-creds = service_account.Credentials.from_service_account_file(Config.GOOGLE_KEY_FILE, scopes=Config.SHEETS_SCOPES) # here enter the name of your downloaded JSON file
+creds = service_account.Credentials.from_service_account_file(Config.GOOGLE_KEY_FILE, scopes=Config.SHEETS_SCOPES)
 
 service = build('sheets', 'v4', credentials=creds)
 
@@ -22,7 +23,7 @@ sheet = service.spreadsheets()
 result_input = sheet.values().get(spreadsheetId=Config.SPREADSHEET_ID,
                                 range=Config.RANGE_NAME).execute()
 
-print(result_input.get('values', []))
+#print(result_input.get('values', []))
 
 i = 0
 names = [line[i] for line in  result_input.get('values', [])]
@@ -38,19 +39,21 @@ if Config.HAS_ADDRESS:
     addresses = [line[i] for line in  result_input.get('values', [])]
 
 i += 1
-messages = [line[i] for line in  result_input.get('values', [])]
+secretMessages = [line[i] for line in  result_input.get('values', [])]
+
 
 if Config.HAS_CONFLICT_MANAGEMENT:
     i += 1
-    conflicts = [[names.index(name) for name in (line[i].split(',') if len(line) > i else [])] for line in result_input.get('values', [])]
+
+conflicts = [[names.index(name) for name in (line[i].split(',') if Config.HAS_CONFLICT_MANAGEMENT and len(line) > i else [])] for line in result_input.get('values', [])]
+
+for c in range(n):
+    conflicts[c] += [c]
     
-    for c in range(n):
-        conflicts[c] += [c]
-    
-    print(conflicts)
+print(conflicts)
 
 
-print("This script will send a DM to the " + str(n) + " following people.")
+print("This script will send a DM to the " + str(n) + " following people:")
 for name in names:
     print(name)
 
@@ -58,72 +61,72 @@ if Config.DRY_RUN:
     print('[THIS IS A DRY RUN]')
 
 if input("Do you want to proceed?(yes/no)") != "yes":
-    print("Aborting")
+    print("Aborting...")
     sys.exit()
 
+graph = np.zeros((2*n+2, 2*n+2))
+hat = [i for i in range(n)]
+giftees = [-1]*n
+random.shuffle(hat)
+for i in range(n):
+    graph[0][i+1] = 1
+    graph[n+i+1][2*n+1] = 1
+    for j in range(n):
+        if j not in conflicts[i]:
+            graph[hat[i]+1][n+j+1] = 1
 
-if Config.HAS_CONFLICT_MANAGEMENT:
-    # TODO Better algorithm for conflict management
-    ok = False
-    hat = [i for i in range(n)]
-    giftees = [-1] * n
-
-    while not ok:
-        ok = True
-        hat = sorted(hat, key=lambda x: random.random())
-        for i in range(n):
-            if hat[(i+1) % n] in conflicts[hat[i]]:
-                print(f'conflict: {names[hat[i]]} cannot gift to {names[hat[(i+1)%n]]}, retrying')
-                ok = False
-                break
-    
-    for i in range(n):
-        giftees[hat[i]] = hat[((i+1) % n)]
-    
+flow = maxflow(graph,0,2*n+1)
+if flow < n:
+    print("Too many constraints, problem is unsolvable\nAborting...")
+    sys.exit()
 else:
-	ok = False
-	while not ok:
-	    giftees=[i for i in range(n)]
-	    ok = True
-	    for i in range(n-1):
-	        j = random.randint(i,n-1)
-	        if giftees[j] == i:
-	            ok = False
-	            break
-	        else:
-	            giftees[i],giftees[j] = giftees[j], giftees[i]
-	    if giftees[n-1] == n-1:
-	        ok = False
+    message = ["" for _ in range(n)]
+    for i in range(n):
+        giftees[i] = hat.index(np.where(graph[i+n+1] == 1)[0][0]-1)
+
+        message[i] = f'Your giftee is ||{names[giftees[i]]}|| !\n'
+        
+        if Config.HAS_ADDRESS:
+            message[i] += f'Their address is:\n||{addresses[giftees[i]]}||\n'
+        
+        if secretMessages[giftees[i]]:
+            message[i] += f'They left the following message for you: \n||{secretMessages[giftees[i]]}||\n'
+        
+        if Config.DRY_RUN:
+            print(f'[MESSAGE FOR {names[i]}]')
+            print(message)
 
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     #for m in client.get_all_members():
     #    print(m)
+    fail = False
+    successful_users = []
     for i in range(n):
         user = discord.utils.get(client.get_all_members(), name=names[i][:-5], discriminator=names[i][-4:])
-        message = f'Your giftee is ||{names[giftees[i]]}|| !\n'
-        
-        if Config.HAS_ADDRESS:
-            message += f'Their address is:\n||{addresses[giftees[i]]}||\n'
-        
-        m = messages[giftees[i]]
-        if m:
-            message += f'They left the following message for you: \n||{messages[giftees[i]]}||\n'
-
-        if Config.DRY_RUN:
-            print(f'[MESSAGE FOR {names[i]}]')
-            print(message)
+        if user is None:
+            print("user " + names[i] + " not found", file=sys.stderr)
+            fail = True
         else:
-            if user is None:
-                print("user " + user + " not found", file=sys.stderr)
-                print(message)
+            try:
+                await user.send("Matching is about to begin, please wait")
+            except:
+                print("Could not DM user " + names[i])
+                fail = True
             else:
-                user.send(message)
+                successful_users+=[user]
+    if fail:
+        print("Aborting...")
+        for u in successful_users:
+            await u.send("An error occurred during matching, so it has been aborted.")
 
-    print("Done!")
+    else:
+        for i in range(len(successful_users)):
+            await successful_users[i].send(message[i])
+        print("Done!")
     await client.close()
 
-#ENTER THE TOKEN OF YOUR DISCORD BOT HERE
-client.run(Config.DISCORD_BOT_TOKEN)
+if not Config.DRY_RUN:
+    client.run(Config.DISCORD_BOT_TOKEN)
 
---
+++ maxflow.py
@@ -0,0 +1,52 @@
+import math
+
+def find_next_node (graph, source, sink, forbidden):
+    for i in range(len(graph[source])):
+        if graph[source][i] > 0 and i not in forbidden:
+            path = find_path(graph, i, sink, forbidden)
+            if path is not None:
+                return i, path
+    return None, None
+
+
+def find_path(graph, source, sink, forbidden):
+    if graph[source][sink] > 0:
+        return [source, sink]
+    else:
+        next_node, path = find_next_node(graph, source, sink, forbidden + [source])
+        if next_node:
+            return [source] + path
+        else:
+            return None
+
+def min_flow(graph, path):
+    flow = math.inf
+    for i in range(len(path)-1):
+        flow = min(flow, graph[path[i]][path[i+1]])
+    return flow
+
+def add_path_flow(graph, path, flow):
+    for i in range(len(path)-1):
+        graph[path[i]][path[i+1]] -= flow
+        graph[path[i+1]][path[i]] += flow
+
+def maxflow(graph,source,sink):
+    path = find_path(graph,source,sink,[])
+    total_flow = 0
+    while path is not None:
+        flow = min_flow(graph, path)
+        """
+        print("Path:")
+        print(path)
+        print("Min flow:")
+        print(flow)
+        """
+        add_path_flow(graph, path, flow)
+        total_flow += flow
+        path = find_path(graph,source,sink,[])
+    """
+    print("Total flow:")
+    print(total_flow)
+    """
+    return total_flow
+
